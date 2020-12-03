0ee33ce67f20a2a36b927402b5f8aa8c61f5e18f
Eugene Moon <moonyoojin@gmail.com>
2020-12-03 01:37:32 -0500

    Add solution for day 3

---
+++ day_03/part_01.js
@@ -0,0 +1,22 @@
+const fs = require('fs').promises;
+
+const parseLines = async () => {
+  const data = await fs.readFile('./input', {encoding: 'utf-8'});
+  return data.split('\r\n');
+};
+
+const solve = async () => {
+  const lines = await parseLines();
+  const size = lines[0].length;
+  let trees = 0;
+  let pos = 0;
+  lines.forEach(line => {
+    if (line[pos] === "#") {
+      trees +=1 ;
+    }
+    pos = (pos + 3) % size;
+  });
+  return trees;
+}
+
+solve().then(console.log);
---
+++ day_03/part_02.js
@@ -0,0 +1,34 @@
+const fs = require('fs').promises;
+
+const parseLines = async () => {
+  const data = await fs.readFile('./input', {encoding: 'utf-8'});
+  return data.split('\r\n');
+};
+
+const solve = async () => {
+  const vectors = [ [1,1], [3,1], [5,1], [7,1], [1,2] ];
+  const lines = await parseLines();
+  const size = lines[0].length;
+  let allTrees = [];
+  
+  vectors.forEach( vector => {
+    let posX = 0, posY = 0, trees = 0;
+    const [right, down] = [...vector];
+    while (posY < lines.length) {
+      let line = lines[posY];
+
+      if (line[posX] === "#") {
+        trees +=1 ;
+      }
+
+      posX = (posX + right) % size;
+      posY = posY + down;
+    }
+
+    allTrees.push(trees);
+  });
+
+  return allTrees.reduce((acc, el) => acc * el);
+}
+
+solve().then(console.log);
